d01ea2d77f58db73c3e2cef06d3ac4ddc3c072b3
Anton Le <le@physics.msu.ru>
2015-07-05 13:00:47 +0300

    Initial commit

---
+++ src/main/java/me/antonle/telegrambot/itbot/ItbotApplication.java
@@ -0,0 +1,12 @@
+package me.antonle.telegrambot.itbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItbotApplication.class, args);
+    }
+}
---
+++ src/test/java/me/antonle/telegrambot/itbot/ItbotApplicationTests.java
@@ -0,0 +1,18 @@
+package me.antonle.telegrambot.itbot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ItbotApplication.class)
+@WebAppConfiguration
+public class ItbotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
