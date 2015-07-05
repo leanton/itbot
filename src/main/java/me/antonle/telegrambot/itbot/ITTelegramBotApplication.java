718607b6fc25e9a4da0b471be3f1d48d6331f392
Anton Le <le@physics.msu.ru>
2015-07-05 18:09:22 +0300

    Itbot-2 Model: addintional configs to save token #2

--- src/main/java/me/antonle/telegrambot/itbot/ITTelegramBotApplication.java
+++ src/main/java/me/antonle/telegrambot/itbot/ITTelegramBotApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ItbotApplication {
+public class ITTelegramBotApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ItbotApplication.class, args);
+        SpringApplication.run(ITTelegramBotApplication.class, args);
     }
 }
