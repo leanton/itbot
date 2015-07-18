73214aa7e5b5b6070d1c725f4fb5eb5b932a715b
Anton Le <le@physics.msu.ru>
2015-07-18 15:55:05 +0300

    #13 Disruptor to handle events - disruptor implemented

--- src/main/java/me/antonle/telegrambot/itbot/properties/BotProperties.java
+++ src/main/java/me/antonle/telegrambot/itbot/properties/BotProperties.java
@@ -1,12 +1,15 @@
-package me.antonle.telegrambot.itbot;
+package me.antonle.telegrambot.itbot.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.NotNull;
 
 @Component
 @ConfigurationProperties("bot")
 public class BotProperties {
 
+    @NotNull
     private String token;
 
     public String getToken() {
@@ -18,9 +21,6 @@ public class BotProperties {
     }
 
     public String getApi() {
-        if (token == null) {
-            throw new NullPointerException("token should be populated");
-        }
         return "https://api.telegram.org/bot" + token;
     }
 }
